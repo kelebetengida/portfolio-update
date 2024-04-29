f071a9c7f93ae383914a2c944dad41b10dfee098
Kelebet Engida <engida398@gmail.com>
2024-04-29 06:18:39 -0700

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Kelebet_Engida',
     title: "<Full Stack Developer & Instructor/>",
     desciption:"Dedicated and efficient Full-stack Agile Developer with an Environmental Science degree  (focusing on remote sensing data analytical skills) from the University of Washington. Have intermediate experience in Python (panda library). Well-rounded professional with communication and organizational skills. I really enjoy using my skill sets to support the entrepreneurial culture of startups and big companies.",
-    image: "https://media.licdn.com/dms/image/D4E03AQH3AvkASjhvgQ/profile-displayphoto-shrink_800_800/0/1668991749436?e=2147483647&v=beta&t=UI375hmT1UMheMkBVDG8kbiROLXBtKUoXqoslajYq1Y",
+    image: "https://media.licdn.com/dms/image/D5603AQEFVXw6FsklpQ/profile-displayphoto-shrink_400_400/0/1701226381520?e=1720051200&v=beta&t=p6qqlPEaj9X-itVz8V6LEgzcrtJhFucWDjiaxazXP3A",
     resumePdf: resume
 }
